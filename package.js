028e423b391218f77dd6deab99d7f084315fbdbc
何海亮 <llwoll@126.com>
2015-12-29 01:59:38 +0800

    support llwoll-gpio

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name: 'juliancwirko:arduino-cylon',
+    name: 'llwoll:arduino-cylon',
     summary: 'Meteor and Cylon.js for Arduino',
     version: '1.1.1',
     git: 'https://github.com/llwoll/meteor-arduino-cylon.git'
@@ -7,6 +7,7 @@ Package.describe({
 
 Npm.depends({
     'serialport': '1.3.1',
+    //'serialport': '2.0.5 ',
     'cylon': '1.0.0',
     'cylon-firmata': '0.20.0',
     'llwoll-gpio': '0.27.0',
